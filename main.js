4a5fc2411716cc724b176bab54a75b36601c0673
Abdolmaged Alkhulaifi <nitroman2009@hotmail.com>
2019-02-06 19:49:30 +0300

    wow

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const I2C = require('raspi-i2c').I2C;
 var address = 0x38;
 raspi.init(() => {
     const i2c = new I2C();
-    console.log(i2c.readByteSync(address)); // Read one byte from the device at address 18
+    console.log(i2c.readSync(address,0xE502,4)); // Read one byte from the device at address 18
   });
